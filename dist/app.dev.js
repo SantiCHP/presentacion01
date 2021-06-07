55ec6cdf528de9715cc0e9509d279e9d5d68c6d6
SantiCHP <santichadmin@gmail.com>
2021-06-07 01:34:09 +0100

    Add files via upload

--- dist/app.dev.js
+++ dist/app.dev.js
@@ -1,10 +1,1 @@
 "use strict";
-
-function translateFunction() {
-  document.getElementById("puerta-izq").style.transform = "translateX(-100%)";
-  document.getElementById("puerta-izq").style.transition = " all 2s";
-  document.getElementById("boton-puerta").style.transform = "translatey(-500%)";
-  document.getElementById("boton-puerta").style.transition = " all 2s";
-  document.getElementById("puerta-der").style.transform = "translateX(100%)";
-  document.getElementById("puerta-der").style.transition = " all 2s";
-}
