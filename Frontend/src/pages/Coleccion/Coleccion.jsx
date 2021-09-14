fdbd94d7d922f6746c3730c0963bf73142ad5d55
Acoidan1993 <acoidan.t.r@hotmail.com>
2021-09-14 18:23:35 +0100

    con botones

--- Frontend/src/pages/Coleccion/Coleccion.jsx
+++ Frontend/src/pages/Coleccion/Coleccion.jsx
@@ -3,7 +3,26 @@ import React from 'react'
 const Coleccion = () => {
     return (
         <div>
-            <h1>Ñordo de pagina hecha por ñorditos</h1>
+                <div id="contenido2">
+                <div className="cards n01">
+                    <div type="button" className="btn btn-warning btn-sm" data-bs-toggle="modal" data-bs-target="#exampleModal">
+                        +INFO
+                    </div>
+                    <div type="button" className="btn btn-warning btn-sm fas fa-heart" data-bs-toggle="modal" data-bs-target="#exampleModal">
+                    </div>
+                    <div type="button" className="btn btn-warning btn-sm fas fa-award" data-bs-toggle="modal" data-bs-target="#exampleModal">
+                    </div>
+                </div>
+                <div className="cards n02">
+
+                </div>
+                <div className="cards n03">
+
+                </div>
+                <div className="cards n04">
+
+                </div>
+                </div>
         </div>
     )
 }
