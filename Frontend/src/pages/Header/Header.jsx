bb4cc82563a3371eec7dfac0672265b639bedec3
AlexArt25 <alex.art.155@gmail.com>
2021-09-15 19:35:07 +0100

    Eliminada la primera barra de navegacion y retocado el css del botton toggler del nav

--- Frontend/src/pages/Header/Header.jsx
+++ Frontend/src/pages/Header/Header.jsx
@@ -15,35 +15,7 @@ const Header = () => {
     // }
 return (
 <div>
-<<<<<<< HEAD
-    {/* <nav className="navbar navbar-expand-lg navbar-dark bg-dark">
-        <div className="container-fluid">
-        <Link to="/tsundoku/Inicio" className="navbar-brand" id='buttonHome'>TSUNDOKU</Link>
-        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarColor02" aria-controls="navbarColor02" aria-expanded="false" aria-label="Toggle navigation">
-            <span class="navbar-toggler-icon"></span>
-        </button>
-            <div className="collapse navbar-collapse" id="navbarColor02">
-                <ul className="navbar-nav mr-auto" id='menu'>
-=======
-    <nav className="navbar navbar-expand-lg navbar-dark bg-dark w-100 ms-2 mt-2" id="barra">
-        <Link to="/tsundoku/Inicio" id='buttonHome'><a className="navbar-brand ps-3" href="#" id="logo">TSUNDOKU</a></Link>
-            <div className="collapse navbar-collapse" id="navbarColor02">
-                <ul className="navbar-nav" id='menu'>
->>>>>>> ef68d40abcdf2f4ec87f140c5b351c06716dd421
-
-                <li className="nav-item"><Link to="/tsundoku/Inicio" className="nav-link" id='buttonHome'>Inicio</Link></li>
-
-                <li className="nav-item"><Link to="/tsundoku/Galeria" className="nav-link" id='buttonHome'>Galería</Link></li>
-
-                <li className="nav-item"><Link to="/tsundoku/Coleccion" className="nav-link" id='buttonHome'>Colección</Link></li>
-
-                <li className="nav-item"><Link to="/tsundoku/Logout" id='buttonHome' onClick={logout()}><a className="nav-link" href="#">Log Out</a></Link></li>
-
-                </ul>
-            </div>
-<<<<<<< HEAD
-        </div>
-    </nav> */}
+    
     <Navbar bg="dark" expand="lg">
   {/* <Container> */}
     <Navbar.Brand><Link to="/tsundoku/Inicio" className="navbar-brand" id='logo'>TSUNDOKU</Link></Navbar.Brand>
@@ -58,9 +30,6 @@ return (
     </Navbar.Collapse>
   {/* </Container> */}
 </Navbar>
-=======
-    </nav>
->>>>>>> ef68d40abcdf2f4ec87f140c5b351c06716dd421
 </div>
 )
 }
