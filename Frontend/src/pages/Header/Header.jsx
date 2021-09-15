8bca98b990a3ceb1459dd91df78692e35c047db0
AlexArt25 <alex.art.155@gmail.com>
2021-09-15 18:54:14 +0100

    Navbar actualizado con dropdown SIN los 5rem,repito SIIN los 5 REM

--- Frontend/src/pages/Header/Header.jsx
+++ Frontend/src/pages/Header/Header.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import { Link, Redirect } from 'react-router-dom';
+import {Nav,Navbar} from "react-bootstrap";
 import './Header.css';
 
 const Header = () => {
@@ -14,24 +15,41 @@ const Header = () => {
     // }
 return (
 <div>
-    <nav className="navbar navbar-expand-lg navbar-dark bg-dark">
+    {/* <nav className="navbar navbar-expand-lg navbar-dark bg-dark">
         <div className="container-fluid">
-        <Link to="/tsundoku/Inicio" id='buttonHome'><a className="navbar-brand" href="#">TSUNDOKU</a></Link>
+        <Link to="/tsundoku/Inicio" className="navbar-brand" id='buttonHome'>TSUNDOKU</Link>
+        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarColor02" aria-controls="navbarColor02" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+        </button>
             <div className="collapse navbar-collapse" id="navbarColor02">
-                <ul className="navbar-nav me-auto" id='menu'>
+                <ul className="navbar-nav mr-auto" id='menu'>
 
-                <li className="nav-item"><Link to="/tsundoku/Inicio" id='buttonHome'><a className="nav-link" href="#">Inicio</a></Link></li>
+                <li className="nav-item"><Link to="/tsundoku/Inicio" className="nav-link" id='buttonHome'>Inicio</Link></li>
 
-                <li className="nav-item"><Link to="/tsundoku/Galeria" id='buttonHome'><a className="nav-link" href="#">Galería</a></Link></li>
+                <li className="nav-item"><Link to="/tsundoku/Galeria" className="nav-link" id='buttonHome'>Galería</Link></li>
 
-                <li className="nav-item"><Link to="/tsundoku/Coleccion" id='buttonHome'><a className="nav-link" href="#">Colección</a></Link></li>
+                <li className="nav-item"><Link to="/tsundoku/Coleccion" className="nav-link" id='buttonHome'>Colección</Link></li>
 
-                {/* <li className="nav-item"><Link to="/tsundoku/Logout" id='buttonHome' onClick={logout()}><a className="nav-link" href="#">Log Out</a></Link></li> */}
+                <li className="nav-item"><Link to="/tsundoku/Logout" id='buttonHome' onClick={logout()}><a className="nav-link" href="#">Log Out</a></Link></li>
 
                 </ul>
             </div>
         </div>
-    </nav>
+    </nav> */}
+    <Navbar bg="dark" expand="lg">
+  {/* <Container> */}
+    <Navbar.Brand><Link to="/tsundoku/Inicio" className="navbar-brand" id='logo'>TSUNDOKU</Link></Navbar.Brand>
+    <Navbar.Toggle aria-controls="basic-navbar-nav" className="buttonHome" />
+    <Navbar.Collapse id="basic-navbar-nav">
+      <Nav className="me-auto">
+        <Nav.Link><Link to="/tsundoku/Inicio" className="nav-link" id='buttonHome'>Inicio</Link></Nav.Link>
+        <Nav.Link><Link to="/tsundoku/Galeria" className="nav-link" id='buttonHome'>Galeria</Link></Nav.Link>
+        <Nav.Link><Link to="/tsundoku/Coleccion" className="nav-link" id='buttonHome'>Conleccion</Link></Nav.Link>
+        {/* <Nav.Link><Link to="/tsundoku/Logout" className="nav-link" id='buttonHome'>Logout</Link></Nav.Link> */} 
+      </Nav>
+    </Navbar.Collapse>
+  {/* </Container> */}
+</Navbar>
 </div>
 )
 }
